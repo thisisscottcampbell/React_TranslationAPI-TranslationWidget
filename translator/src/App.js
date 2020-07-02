9bcfac6507a1e3b8ae86830801f87b89bdfb0d67
Scott Campbell <stc9486@gmail.com>
2020-07-02 16:28:02 -0400

    set

--- translator/src/App.js
+++ translator/src/App.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Field from './components/Field'
 import Languages from './components/Languages'
 import Translate from './components/Translate'
 
 const App = () => {
+  const [language, setLanguage] = useState('ru');
+
   return (
     <div className="App">
-      <Field />
-      <Languages />
+      <Field label="Enter English" onChange={setText} value={text} />
+      <Languages language={language} onLanguageChange={setLanguage} />
       <hr />
-      <Translate />
+      <Translate text={text} language={language} />
     </div>
   );
 }
